3e784fc5cccaf02597a6ed7eb218cd238cb04f9e
amira-saya <amirasayari9@gmail.com>
2020-12-10 22:36:16 -0800

    JSX CP

---
+++ src/App.js
@@ -0,0 +1,39 @@
+import React from 'react';
+//..import logo from './logo.svg';
+//import './App.css';
+//import imageInSrc from './imageInSrc.jpg'
+//import myVideo from './myVideo.mp4'
+//import css from './style.css'
+
+//function App() {
+//return (
+  //  <>
+    
+   // <h1 className="title red">welcome</h1>
+   // <div>
+//
+    //               <img src={imageInSrc} alt='imageInSrc' className="srcImage"/>
+       
+     //               <img src="/imageInPublic.jpg" alt ="imageInPublic" className="publicImage"/>
+   
+  
+   
+   ///<video className="video" width="150" height="150" controls >
+   
+  //  <source src={myVideo.mp4} type="video/mp4" />
+   
+   //</video>
+  // </div>
+   //</>
+ // );
+//}
+
+//export default App;
+const App=()=>{
+  return (
+    <>
+    <h2>hello from my first component</h2>
+</>
+  );
+};
+export default App;
